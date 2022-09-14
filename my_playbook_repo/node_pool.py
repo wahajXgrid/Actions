d2ac4171ca363cec8100e991fa8cecdefe31296d
wahajXgrid <wahaj.ali@xgrid.co>
2022-09-14 13:50:31 +0000

    a

---
+++ my_playbook_repo/node_pool.py
@@ -0,0 +1,30 @@
+from robusta.api import *
+
+@action
+def node_pool(event: ExecutionBaseEvent): # We use EventEvent to get the event object.
+    #actual_event = event.get_event()
+    pod = pod_create()
+
+
+
+# returns a pod that mounts the given persistent volume
+
+def pod_create():
+    reader_pod_spec = RobustaPod(
+        apiVersion="v1",
+        kind="Pod",
+        metadata=ObjectMeta(
+            name="volume-inspector",      
+        ),
+        spec=PodSpec(
+            containers=[
+                Container(
+                    name="nginx",
+                    image="nginx",
+                  
+                )
+            ]
+        )
+    )
+    reader_pod = reader_pod_spec.create()
+    return reader_pod
