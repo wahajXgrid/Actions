21ffaf08ef09564540583f31ada3bb8e40e1f93f
wahajXgrid <wahaj.ali@xgrid.co>
2022-09-15 14:56:30 +0000

    a

--- my_playbook_repo/node_pool.py
+++ my_playbook_repo/node_pool.py
@@ -1,33 +1,25 @@
-from email.policy import default
+
 from robusta.api import *
+from google.cloud.container import NodePool, GetClusterRequest, GetServerConfigRequest, GetNodePoolRequest
+from googleapiclient import discovery
+import google.auth
+
 
 @action
-def node_pool(event: ExecutionBaseEvent): # We use EventEvent to get the event object.
+# We use EventEvent to get the event object.
+def node_pool(event: ExecutionBaseEvent):
     #actual_event = event.get_event()
-    pod = pod_create()
+    nodepool = GetNodePoolRequest()
+    g_creds = google.auth.default()
+    service = discovery.build('container', 'v1', credentials=g_creds)
 
+    gcp_projects = ['wahajnodepool']
 
+    for project in gcp_projects:
+        request = service.projects().zones().clusters().list(projectId=project, zone='-')
+        response = request.execute()
 
-# returns a pod that mounts the given persistent volume
-
-def pod_create():
-    reader_pod_spec = RobustaPod(
-        apiVersion="v1",
-        kind="Pod",
-        metadata=ObjectMeta(
-            name="volume-inspector",
-            namespace="default",
-            
-        ),
-        spec=PodSpec(
-            containers=[
-                Container(
-                    name="nginx",
-                    image="nginx",
-                  
-                )
-            ],
-        )
-    )
-    reader_pod = reader_pod_spec.create()
-    return reader_pod
+        if 'clusters' in response:
+            for cluster in response['clusters']:
+                print("%s,%s,%d" %
+                      (project, cluster['name'], cluster['currentNodeCount']))
