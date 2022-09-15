c6ce00ee8618a70e65c556e3e2756d469ff241a6
wahajXgrid <wahaj.ali@xgrid.co>
2022-09-15 20:11:17 +0000

    updated

--- my_playbook_repo/node_pool.py
+++ my_playbook_repo/node_pool.py
@@ -3,29 +3,46 @@ from robusta.api import *
 import google.auth
 from google.cloud.container_v1 import ClusterManagerClient
 from kubernetes import client
+from google.cloud import container_v1beta1
 
 
 @action
-def node_pool(event: ExecutionBaseEvent):
+def sample_get_cluster():
+    # Create a client
+    client = container_v1beta1.ClusterManagerClient()
 
-    credentials, project = google.auth.default(
-        scopes=['https://www.googleapis.com/auth/cloud-platform'])
+    # Initialize request argument(s)
+    request = container_v1beta1.GetClusterRequest(
+        project_id="project_id_value",
+        zone="zone_value",
+        cluster_id="cluster_id_value",
+    )
 
-    credentials.refresh(google.auth.transport.requests.Request())
-    cluster_manager = ClusterManagerClient(credentials=credentials)
-    cluster = cluster_manager.get_cluster(
-        zone='us-central1-c', project_id=project, name='nodepool')
+    # Make the request
+    response = client.get_cluster(request=request)
+
+    # Handle the response
+    print(response)
+# def node_pool(event: ExecutionBaseEvent):
+
+#     credentials, project = google.auth.default(
+#         scopes=['https://www.googleapis.com/auth/cloud-platform'])
+
+#     credentials.refresh(google.auth.transport.requests.Request())
+#     cluster_manager = ClusterManagerClient(credentials=credentials)
+#     cluster = cluster_manager.get_cluster(
+#         zone='us-central1-c', project_id=project, name='nodepool')
     
-    config = client.Configuration()
-    config.host = f'https://{cluster.endpoint}:443'
-    config.verify_ssl = False
-    config.api_key = {"authorization": "Bearer " + credentials.token}
-    config.username = credentials._service_account_email
+#     config = client.Configuration()
+#     config.host = f'https://{cluster.endpoint}:443'
+#     config.verify_ssl = False
+#     config.api_key = {"authorization": "Bearer " + credentials.token}
+#     config.username = credentials._service_account_email
 
-    client.Configuration.set_default(config)
+#     client.Configuration.set_default(config)
 
-    kub = client.CoreV1Api()
-    print(kub.list_pod_for_all_namespaces(watch=False))
+#     kub = client.CoreV1Api()
+#     print(kub.list_pod_for_all_namespaces(watch=False))
     # #g_creds = google.auth.default()
     # service = discovery.build('container', 'v1', credentials=g_creds)
 
