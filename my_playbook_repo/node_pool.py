ff7e923a3bdba68fcc98cedbc8b75e4e05d09529
wahajXgrid <wahaj.ali@xgrid.co>
2022-09-17 14:28:13 +0000

    updated

--- my_playbook_repo/node_pool.py
+++ my_playbook_repo/node_pool.py
@@ -1,69 +1,69 @@
-# import pulumi
-# import pulumi_gcp
+import pulumi
+import pulumi_gcp as gcp
 
-# # Read in some configurable settings for our cluster.
-# # If nothing is set the specified default values will take effect.
-# config = pulumi.Config()
-# NODE_COUNT = config.get_int('node_count') or 1
-# NODE_MACHINE_TYPE = config.get('node_machine_type') or 'e2-medium'
-# MASTER_VERSION = config.get('master_version') or '1.21.14-gke.700'
+# Read in some configurable settings for our cluster.
+# If nothing is set the specified default values will take effect.
+config = pulumi.Config()
+NODE_COUNT = config.get_int('node_count') or 1
+NODE_MACHINE_TYPE = config.get('node_machine_type') or 'e2-medium'
+MASTER_VERSION = config.get('master_version') or '1.21.14-gke.700'
 
-# # Defining the GKE Cluster
-# gke_cluster = gcp.container.Cluster('cluster-1', 
-#     name = "cluster-1",
-#     location = "us-central1",
-#     initial_node_count = NODE_COUNT,
-#     remove_default_node_pool = True,
-#     min_master_version = MASTER_VERSION,
-# )
+# Defining the GKE Cluster
+gke_cluster = gcp.container.Cluster('cluster-1', 
+    name = "cluster-1",
+    location = "us-central1",
+    initial_node_count = NODE_COUNT,
+    remove_default_node_pool = True,
+    min_master_version = MASTER_VERSION,
+)
 
-# # Defining the GKE Node Pool
-# gke_nodepool = gcp.container.NodePool("nodepool-1",
-#     name = "nodepool-1",
-#     location = "us-central1",
-#     node_locations = ["us-central1-a"],
-#     cluster = gke_cluster.id,
-#     node_count = NODE_COUNT,
-#     node_config = gcp.container.NodePoolNodeConfigArgs(
-#         preemptible = False,
-#         machine_type = NODE_MACHINE_TYPE,
-#         disk_size_gb = 20,
-#         oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"],
-#         shielded_instance_config = gcp.container.NodePoolNodeConfigShieldedInstanceConfigArgs(
-#             enable_integrity_monitoring = True,
-#             enable_secure_boot = True
-#         )
-#     ),
-#     # Set the Nodepool Autoscaling configuration
-#     autoscaling = gcp.container.NodePoolAutoscalingArgs(
-#         min_node_count = 1,
-#         max_node_count = 3
-#     ),
-#     # Set the Nodepool Management configuration
-#     management = gcp.container.NodePoolManagementArgs(
-#         auto_repair  = True,
-#         auto_upgrade = True
-#     )
-# )
-
-
+# Defining the GKE Node Pool
+gke_nodepool = gcp.container.NodePool("nodepool-1",
+    name = "nodepool-1",
+    location = "us-central1",
+    node_locations = ["us-central1-a"],
+    cluster = gke_cluster.id,
+    node_count = NODE_COUNT,
+    node_config = gcp.container.NodePoolNodeConfigArgs(
+        preemptible = False,
+        machine_type = NODE_MACHINE_TYPE,
+        disk_size_gb = 20,
+        oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"],
+        shielded_instance_config = gcp.container.NodePoolNodeConfigShieldedInstanceConfigArgs(
+            enable_integrity_monitoring = True,
+            enable_secure_boot = True
+        )
+    ),
+    # Set the Nodepool Autoscaling configuration
+    autoscaling = gcp.container.NodePoolAutoscalingArgs(
+        min_node_count = 1,
+        max_node_count = 3
+    ),
+    # Set the Nodepool Management configuration
+    management = gcp.container.NodePoolManagementArgs(
+        auto_repair  = True,
+        auto_upgrade = True
+    )
+)
 
 
 
-from robusta.api import *
-import google.cloud.container_v1 as container
-from google.auth import compute_engine
-from google.cloud.container_v1 import ClusterManagerClient
-from kubernetes import client, config
 
-@action
-def node_pool(event: ExecutionBaseEvent):
-    project_id = 'wahajnodepool'
-    zone = 'us-central1-c'
-    cluster_id = 'nodepool'
 
-    credentials = compute_engine.Credentials()
-    print("****",credentials)
+# from robusta.api import *
+# import google.cloud.container_v1 as container
+# from google.auth import compute_engine
+# from google.cloud.container_v1 import ClusterManagerClient
+# from kubernetes import client, config
+
+# @action
+# def node_pool(event: ExecutionBaseEvent):
+#     project_id = 'wahajnodepool'
+#     zone = 'us-central1-c'
+#     cluster_id = 'nodepool'
+
+#     credentials = compute_engine.Credentials()
+#     print("****",credentials)
     
     # gclient: ClusterManagerClient = container.ClusterManagerClient(credentials=credentials)
 
