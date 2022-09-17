db9abd8ec51e4a59d044ed1374d2dd205eb4f1f3
wahajXgrid <wahaj.ali@xgrid.co>
2022-09-17 15:09:09 +0000

    updated

--- my_playbook_repo/node_pool.py
+++ my_playbook_repo/node_pool.py
@@ -1,6 +1,6 @@
 import gcloud_config_helper
 from robusta.api import *
-from google.cloud import compute_v1
+from google.cloud.compute_v1.types import compute_v1
 # import pulumi
 # import pulumi_gcp as gcp
 
