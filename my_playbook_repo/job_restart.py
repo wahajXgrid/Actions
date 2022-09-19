275a4a2470b9d430bb2bf629414df7c22140b7b3
wahajXgrid <wahaj.ali@xgrid.co>
2022-09-19 13:06:09 +0000

    updated

--- my_playbook_repo/job_restart.py
+++ my_playbook_repo/job_restart.py
@@ -4,9 +4,9 @@ from robusta.api import *
 @action
 def job_restart(event: JobEvent):
     job_event = event.get_job()
-    print(job_event)
-    deep_copy = deepcopy(job_event)
-    job_event.delete()
-    print('*******')
-    print(deep_copy)
-    deep_copy.create()
+    print(job_event.spec)
+    # deep_copy = deepcopy(job_event)
+    # job_event.delete()
+    # print('*******')
+    # print(deep_copy)
+    # deep_copy.create()
