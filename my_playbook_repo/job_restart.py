ad6539fb14fdb7e59ea0941000460916cccb3e58
wahajXgrid <wahaj.ali@xgrid.co>
2022-09-19 13:55:49 +0000

    updated

--- my_playbook_repo/job_restart.py
+++ my_playbook_repo/job_restart.py
@@ -1,19 +1,36 @@
 from copy import copy
+from pdb import Restart
 from robusta.api import *
+
 
 @action
 def job_restart(event: JobEvent):
     job_event = event.get_job()
-   
+
     # deep_copy = deepcopy(job_event)
     # job_event.delete()
     # print('*******')
     # print(deep_copy)
     # deep_copy.create()
     job_spec = RobustaJob(
-        metadata= job_event.metadata,
-        spec= job_event.spec,
-    
+        metadata=ObjectMeta(
+            name=job_event.metadata.name,
+            namespace=job_event.metadata.namespace,
+            labels=job_event.metadata.labels,
+        ),
+        spec=JobSpec(
+            backoffLimit=job_event.spec.backoffLimit,
+            template=PodTemplateSpec(
+                spec=PodSpec(
+                    containers=[Container(
+                        name=job_event.spec.template.spec.containers[0].name,
+                        image=job_event.spec.template.spec.containers[0].image,
+                    ),
+                    ],
+                ),
+            ),
+
+        ),
     )
     job_event.delete()
     print(job_spec)
