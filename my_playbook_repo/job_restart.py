e4ab4c1c8ba83573cd5d0ca7d91a619359e1d18f
wahajXgrid <wahaj.ali@xgrid.co>
2022-09-20 11:28:16 +0000

    updated

--- my_playbook_repo/job_restart.py
+++ my_playbook_repo/job_restart.py
@@ -6,7 +6,7 @@ from robusta.api import *
 
 @action
 def job_restart(event: JobEvent, params: EventEnricherParams):
-    job_event = event.get_context()
+    job_event = event.get_resource()
     print ("*****************")
     print(job_event)
     # job_spec = RobustaJob(
