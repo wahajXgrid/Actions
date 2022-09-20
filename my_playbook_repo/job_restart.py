866932085030ca73024a378474f65bce6e2ad941
wahajXgrid <wahaj.ali@xgrid.co>
2022-09-20 11:24:16 +0000

    updated

--- my_playbook_repo/job_restart.py
+++ my_playbook_repo/job_restart.py
@@ -3,42 +3,44 @@ from pdb import Restart
 from robusta.api import *
 
 
+
 @action
 def job_restart(event: JobEvent, params: EventEnricherParams):
     job_event = event.get_job()
-    job_spec = RobustaJob(
-        metadata=ObjectMeta(
-            name=job_event.metadata.name,
-            namespace=job_event.metadata.namespace,
-            labels=job_event.metadata.labels,
-        ),
-        spec=JobSpec(
-            completions=job_event.spec.completions,
-            parallelism=job_event.spec.parallelism,
-            backoffLimit=job_event.spec.backoffLimit,
+    print(job_event)
+    # job_spec = RobustaJob(
+    #     metadata=ObjectMeta(
+    #         name=job_event.metadata.name,
+    #         namespace=job_event.metadata.namespace,
+    #         labels=job_event.metadata.labels,
+    #     ),
+    #     spec=JobSpec(
+    #         completions=job_event.spec.completions,
+    #         parallelism=job_event.spec.parallelism,
+    #         backoffLimit=job_event.spec.backoffLimit,
            
-            activeDeadlineSeconds=job_event.spec.activeDeadlineSeconds,
+    #         activeDeadlineSeconds=job_event.spec.activeDeadlineSeconds,
             
-            ttlSecondsAfterFinished=job_event.spec.ttlSecondsAfterFinished,
-            template=PodTemplateSpec(
-                spec=PodSpec(
-                    containers=[Container(
-                        name=job_event.spec.template.spec.containers[0].name,
-                        image=job_event.spec.template.spec.containers[0].image,
-                        args=job_event.spec.template.spec.containers[0].args,
-                        command=job_event.spec.template.spec.containers[0].command,
-                        env=job_event.spec.template.spec.containers[0].env,
-                        envFrom=job_event.spec.template.spec.containers[0].envFrom,
-                        imagePullPolicy=job_event.spec.template.spec.containers[0].imagePullPolicy,
+    #         ttlSecondsAfterFinished=job_event.spec.ttlSecondsAfterFinished,
+    #         template=PodTemplateSpec(
+    #             spec=PodSpec(
+    #                 containers=[Container(
+    #                     name=job_event.spec.template.spec.containers[0].name,
+    #                     image=job_event.spec.template.spec.containers[0].image,
+    #                     args=job_event.spec.template.spec.containers[0].args,
+    #                     command=job_event.spec.template.spec.containers[0].command,
+    #                     env=job_event.spec.template.spec.containers[0].env,
+    #                     envFrom=job_event.spec.template.spec.containers[0].envFrom,
+    #                     imagePullPolicy=job_event.spec.template.spec.containers[0].imagePullPolicy,
 
-                    ),
-                    ],
-                restartPolicy= job_event.spec.template.spec.restartPolicy
-                ),
-            ),
+    #                 ),
+    #                 ],
+    #             restartPolicy= job_event.spec.template.spec.restartPolicy
+    #             ),
+    #         ),
 
-        ),
-    )
-    job_event.delete() 
-    job_spec.create()
+    #     ),
+    # )
+    # job_event.delete() 
+    # job_spec.create()
 
