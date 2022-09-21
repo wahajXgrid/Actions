0a0496f36eb2682e0fc378d189cfc052798ba001
wahajXgrid <wahaj.ali@xgrid.co>
2022-09-21 17:15:57 +0500

    updated

--- my_playbook_repo/job_restart.py
+++ my_playbook_repo/job_restart.py
@@ -11,6 +11,41 @@ def job_restart(event: JobEvent, params: EventEnricherParams):
         pod = get_job_pod(event.get_job().metadata.namespace, event.get_job().metadata.name)
         if pod.status.containerStatuses[0].state.terminated.reason == 'Error':
             print("han bhai theek hy")
+            job_spec = RobustaJob(
+                metadata=ObjectMeta(
+                    name=job_event.metadata.name,
+                    namespace=job_event.metadata.namespace,
+                    labels=job_event.metadata.labels,
+                ),
+                spec=JobSpec(
+                    completions=job_event.spec.completions,
+                    parallelism=job_event.spec.parallelism,
+                    backoffLimit=job_event.spec.backoffLimit,
+                
+                    activeDeadlineSeconds=job_event.spec.activeDeadlineSeconds,
+                    
+                    ttlSecondsAfterFinished=job_event.spec.ttlSecondsAfterFinished,
+                    template=PodTemplateSpec(
+                        spec=PodSpec(
+                            containers=[Container(
+                                name=job_event.spec.template.spec.containers[0].name,
+                                image=job_event.spec.template.spec.containers[0].image,
+                                args=job_event.spec.template.spec.containers[0].args,
+                                command=job_event.spec.template.spec.containers[0].command,
+                                env=job_event.spec.template.spec.containers[0].env,
+                                envFrom=job_event.spec.template.spec.containers[0].envFrom,
+                                imagePullPolicy=job_event.spec.template.spec.containers[0].imagePullPolicy,
+
+                            ),
+                            ],
+                        restartPolicy= job_event.spec.template.spec.restartPolicy
+                        ),
+                    ),
+
+                ),
+            )
+            job_event.delete() 
+            job_spec.create()
         else:
             print("nh bhai")
         
