576eb1f3823e6868077f45db899df598ae3af6d8
wahajXgrid <wahaj.ali@xgrid.co>
2022-09-21 12:31:04 +0500

    updated

--- my_playbook_repo/job_restart.py
+++ my_playbook_repo/job_restart.py
@@ -10,7 +10,7 @@ def job_restart(event: JobEvent, params: EventEnricherParams):
     if job_event is not None:     
         print ("*****************")
         print("FAILED")
-        print(event.get_job)
+        #print(event.get_job)
     
     else:
         print ("*****************")
