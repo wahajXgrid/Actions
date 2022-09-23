afebac41d978b07add43394f8912b12b952bf648
wahajXgrid <wahaj.ali@xgrid.co>
2022-09-23 16:53:43 +0500

    updated

--- my_playbook_repo/job_restart.py
+++ my_playbook_repo/job_restart.py
@@ -1,4 +1,5 @@
 from ast import Break
+from cgitb import reset
 from copy import copy
 from pdb import Restart
 from robusta.api import *
@@ -27,50 +28,49 @@ def job_restart(event: JobEvent):
     job = event.get_job().status.failed
     job_event = event.get_job()
     if job is not None:
-        print(job_event.spec.template.spec.containers[0].resources.limits)
-        print(job_event.spec.template.spec.containers[0].resources.requests)
-        # # https://docs.robusta.dev/master/developer-guide/actions/findings-api.html
-        # pod = get_job_pod(event.get_job().metadata.namespace,
-        #                   event.get_job().metadata.name)
-        # status_flag = False
-        # for status in pod.status.containerStatuses:
-        #     if status.state.terminated.reason == 'OOMKilled':
-        #         status_flag = True
-        #         break
+      
+        # https://docs.robusta.dev/master/developer-guide/actions/findings-api.html
+        pod = get_job_pod(event.get_job().metadata.namespace,
+                          event.get_job().metadata.name)
+        status_flag = False
+        for status in pod.status.containerStatuses:
+            if status.state.terminated.reason == 'OOMKilled':
+                status_flag = True
+                break
 
-        # if status_flag:
-        #     print("han bhai theek hy")
-        #     #for multi-containers
-        #     container_list = get_container_list(
-        #         job_event.spec.template.spec.containers)
+        if status_flag:
+            print("han bhai theek hy")
+            #for multi-containers
+            container_list = get_container_list(
+                job_event.spec.template.spec.containers)
                 
-        #     job_spec = RobustaJob(
-        #         metadata=ObjectMeta(
-        #             name=job_event.metadata.name,
-        #             namespace=job_event.metadata.namespace,
-        #             labels=job_event.metadata.labels,
-        #         ),
-        #         spec=JobSpec(
-        #             completions=job_event.spec.completions,
-        #             parallelism=job_event.spec.parallelism,
-        #             backoffLimit=job_event.spec.backoffLimit,
-        #             activeDeadlineSeconds=job_event.spec.activeDeadlineSeconds,
-        #             ttlSecondsAfterFinished=job_event.spec.ttlSecondsAfterFinished,
-        #             template=PodTemplateSpec(
-        #                 spec=PodSpec(
-        #                     containers=container_list,
-        #                     restartPolicy=job_event.spec.template.spec.restartPolicy
-        #                 ),
-        #             ),
+            job_spec = RobustaJob(
+                metadata=ObjectMeta(
+                    name=job_event.metadata.name,
+                    namespace=job_event.metadata.namespace,
+                    labels=job_event.metadata.labels,
+                ),
+                spec=JobSpec(
+                    completions=job_event.spec.completions,
+                    parallelism=job_event.spec.parallelism,
+                    backoffLimit=job_event.spec.backoffLimit,
+                    activeDeadlineSeconds=job_event.spec.activeDeadlineSeconds,
+                    ttlSecondsAfterFinished=job_event.spec.ttlSecondsAfterFinished,
+                    template=PodTemplateSpec(
+                        spec=PodSpec(
+                            containers=container_list,
+                            restartPolicy=job_event.spec.template.spec.restartPolicy
+                        ),
+                    ),
 
-        #         ),
-        #     )
-        #     job_event.delete()
-        #     job_spec.create()
+                ),
+            )
+            job_event.delete()
+            job_spec.create()
 
-    # else:
-    #     print("*****************")
-    #     print("Succeed")
+    else:
+        print("*****************")
+        print("Succeed")
 
 
 def get_job_pod(namespace, job):
@@ -91,7 +91,8 @@ def get_container_list(containers_spec):
             env=container.env,
             envFrom=container.envFrom,
             imagePullPolicy=container.imagePullPolicy,
-            resources=container.resources.limit
+            resources=container.resources
+            
 
         ))
     return containers_list
