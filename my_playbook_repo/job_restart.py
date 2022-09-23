28e8f250672c63aba753e6417e92d6333d457ca4
wahajXgrid <wahaj.ali@xgrid.co>
2022-09-23 17:20:51 +0500

    updated

--- my_playbook_repo/job_restart.py
+++ my_playbook_repo/job_restart.py
@@ -29,8 +29,9 @@ def job_restart(event: JobEvent):
     job_event = event.get_job()
     if job is not None:
       
-      
-      print([int(s) for s in (job_event.spec.template.spec.containers[0].resources.limits['memory']).split() if s.isdigit()])
+      txt = job_event.spec.template.spec.containers[0].resources.limits['memory']
+      print (txt)
+      #print([int(s) for s in (job_event.spec.template.spec.containers[0].resources.limits['memory']).split() if s.isdigit()])
     #     # https://docs.robusta.dev/master/developer-guide/actions/findings-api.html
     #     pod = get_job_pod(event.get_job().metadata.namespace,
     #                       event.get_job().metadata.name)
