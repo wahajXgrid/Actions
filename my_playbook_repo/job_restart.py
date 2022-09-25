073496df199a873f153f9687cf73e6bcb1a182a7
wahajXgrid <wahaj.ali@xgrid.co>
2022-09-25 15:14:12 +0500

    updated

--- my_playbook_repo/job_restart.py
+++ my_playbook_repo/job_restart.py
@@ -28,7 +28,7 @@ def job_restart(event: JobEvent):
     job = event.get_job().status.failed
     job_event = event.get_job()
     if job is not None:
-      
+      print(job_event.spec.template.spec.containers[0].resources.limits)
     #   txt = job_event.spec.template.spec.containers[0].resources.limits['memory']
     
     #   num=''
@@ -40,43 +40,43 @@ def job_restart(event: JobEvent):
     #   print(int(num))
 
         # https://docs.robusta.dev/master/developer-guide/actions/findings-api.html
-        pod = get_job_pod(event.get_job().metadata.namespace,
-                          event.get_job().metadata.name)
-        status_flag = False
-        for status in pod.status.containerStatuses:
-            if status.state.terminated.reason == 'OOMKilled':
-                status_flag = True
-                break
+        # pod = get_job_pod(event.get_job().metadata.namespace,
+        #                   event.get_job().metadata.name)
+        # status_flag = False
+        # for status in pod.status.containerStatuses:
+        #     if status.state.terminated.reason == 'OOMKilled':
+        #         status_flag = True
+        #         break
 
-        if status_flag:
-            print("han bhai theek hy")
-            #for multi-containers
-            container_list = get_container_list(
-                job_event.spec.template.spec.containers)
+        # if status_flag:
+        #     print("han bhai theek hy")
+        #     #for multi-containers
+        #     container_list = get_container_list(
+        #         job_event.spec.template.spec.containers)
                 
-            job_spec = RobustaJob(
-                metadata=ObjectMeta(
-                    name=job_event.metadata.name,
-                    namespace=job_event.metadata.namespace,
-                    labels=job_event.metadata.labels,
-                ),
-                spec=JobSpec(
-                    completions=job_event.spec.completions,
-                    parallelism=job_event.spec.parallelism,
-                    backoffLimit=job_event.spec.backoffLimit,
-                    activeDeadlineSeconds=job_event.spec.activeDeadlineSeconds,
-                    ttlSecondsAfterFinished=job_event.spec.ttlSecondsAfterFinished,
-                    template=PodTemplateSpec(
-                        spec=PodSpec(
-                            containers=container_list,
-                            restartPolicy=job_event.spec.template.spec.restartPolicy
-                        ),
-                    ),
+        #     job_spec = RobustaJob(
+        #         metadata=ObjectMeta(
+        #             name=job_event.metadata.name,
+        #             namespace=job_event.metadata.namespace,
+        #             labels=job_event.metadata.labels,
+        #         ),
+        #         spec=JobSpec(
+        #             completions=job_event.spec.completions,
+        #             parallelism=job_event.spec.parallelism,
+        #             backoffLimit=job_event.spec.backoffLimit,
+        #             activeDeadlineSeconds=job_event.spec.activeDeadlineSeconds,
+        #             ttlSecondsAfterFinished=job_event.spec.ttlSecondsAfterFinished,
+        #             template=PodTemplateSpec(
+        #                 spec=PodSpec(
+        #                     containers=container_list,
+        #                     restartPolicy=job_event.spec.template.spec.restartPolicy
+        #                 ),
+        #             ),
 
-                ),
-            )
-            job_event.delete()
-            job_spec.create()
+        #         ),
+        #     )
+        #     job_event.delete()
+        #     job_spec.create()
 
     else:
         print("*****************")
@@ -101,6 +101,6 @@ def get_container_list(containers_spec):
             env=container.env,
             envFrom=container.envFrom,
             imagePullPolicy=container.imagePullPolicy,
-            resources=container.resources
+            
         ))
     return containers_list
