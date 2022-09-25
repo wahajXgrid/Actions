105c9930c3f157b3bdfbdb5d5b8605c0fb6f06d3
wahajXgrid <wahaj.ali@xgrid.co>
2022-09-25 16:49:41 +0500

    updated

--- my_playbook_repo/job_restart.py
+++ my_playbook_repo/job_restart.py
@@ -31,46 +31,47 @@ def job_restart(event: JobEvent):
         # https://docs.robusta.dev/master/developer-guide/actions/findings-api.html
         pod = get_job_pod(event.get_job().metadata.namespace,
                           event.get_job().metadata.name)
-        status_flag = False
-        for status in pod.status.containerStatuses:
-            if status.state.terminated.reason == 'OOMKilled':
-                
-                
-                status_flag = True
-                break
-            else:
-                print("Pod is running")
-
-        if status_flag:
-            print("han bhai theek hy")
-            #for multi-containers
-            container_list = get_container_list(
-                job_event.spec.template.spec.containers)
-                
-            job_spec = RobustaJob(
-                metadata=ObjectMeta(
-                    name=job_event.metadata.name,
-                    namespace=job_event.metadata.namespace,
-                    labels=job_event.metadata.labels,
-                ),
-                spec=JobSpec(
-                    completions=job_event.spec.completions,
-                    parallelism=job_event.spec.parallelism,
-                    backoffLimit=job_event.spec.backoffLimit,
-                    activeDeadlineSeconds=job_event.spec.activeDeadlineSeconds,
-                    ttlSecondsAfterFinished=job_event.spec.ttlSecondsAfterFinished,
-                    template=PodTemplateSpec(
-                        spec=PodSpec(
-                            containers=container_list,
-                            restartPolicy=job_event.spec.template.spec.restartPolicy
-                        ),
-                    ),
-
-                ),
-            )
-            job_event.delete()
+        print(pod.status)
+        # if pod.status
+        #     status_flag = False
+        #     for status in pod.status.containerStatuses:
+        #         if status.state.terminated.reason == 'OOMKilled':
+                    
+                    
+        #             status_flag = True
+        #             break
             
-            job_spec.create()
+
+        #     if status_flag:
+        #         print("han bhai theek hy")
+        #         #for multi-containers
+        #         container_list = get_container_list(
+        #             job_event.spec.template.spec.containers)
+                    
+        #         job_spec = RobustaJob(
+        #             metadata=ObjectMeta(
+        #                 name=job_event.metadata.name,
+        #                 namespace=job_event.metadata.namespace,
+        #                 labels=job_event.metadata.labels,
+        #             ),
+        #             spec=JobSpec(
+        #                 completions=job_event.spec.completions,
+        #                 parallelism=job_event.spec.parallelism,
+        #                 backoffLimit=job_event.spec.backoffLimit,
+        #                 activeDeadlineSeconds=job_event.spec.activeDeadlineSeconds,
+        #                 ttlSecondsAfterFinished=job_event.spec.ttlSecondsAfterFinished,
+        #                 template=PodTemplateSpec(
+        #                     spec=PodSpec(
+        #                         containers=container_list,
+        #                         restartPolicy=job_event.spec.template.spec.restartPolicy
+        #                     ),
+        #                 ),
+
+        #             ),
+        #         )
+        #         job_event.delete()
+                
+        #         job_spec.create()
 
     else:
         print("*****************")
