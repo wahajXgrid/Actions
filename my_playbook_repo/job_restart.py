a1a957feae999205163f2846e6817291e8332691
wahajXgrid <wahaj.ali@xgrid.co>
2022-09-28 17:03:23 +0500

    updated

--- my_playbook_repo/job_restart.py
+++ my_playbook_repo/job_restart.py
@@ -13,66 +13,67 @@ class IncreaseResources(ActionParams):
 @action
 def job_restart(event: JobEvent,params: IncreaseResources):
     job_event = event.get_job()
-    
-    function_name = "job_restart"
-    finding = Finding(
-        title=f"JOB RESTART",
-        source=FindingSource.MANUAL,
-        aggregation_key=function_name,
-        finding_type=FindingType.REPORT,
-        failure=False,
-    )
-    job_temp = event.get_job()
-
-    finding.add_enrichment(
-        [
-            MarkdownBlock(
-                f"*job*restart*\n```\n{job_temp}\n```"
-            ),
-        ]
-    )
-    event.add_finding(finding)
-
-    job_event = event.get_job()
-    pod = get_job_pod(event.get_job().metadata.namespace,
-                        event.get_job().metadata.name)
-    
-    status_flag = False
-    # for multi-containers
-    for status in pod.status.containerStatuses:
-        if status.state.terminated.reason == 'OOMKilled':
-            status_flag = True
-            break
-
-    if status_flag:
-        print("han bhai theek hy")         
-        container_list = get_container_list(
-            job_event.spec.template.spec.containers , increase_to=params.increase_to)
-            
-        job_spec = RobustaJob(
-            metadata=ObjectMeta(
-                name=job_event.metadata.name,
-                namespace=job_event.metadata.namespace,
-                labels=job_event.metadata.labels,
-            ),
-            spec=JobSpec(
-                completions=job_event.spec.completions,
-                parallelism=job_event.spec.parallelism,
-                backoffLimit=job_event.spec.backoffLimit,
-                activeDeadlineSeconds=job_event.spec.activeDeadlineSeconds,
-                ttlSecondsAfterFinished=job_event.spec.ttlSecondsAfterFinished,
-                template=PodTemplateSpec(
-                    spec=PodSpec(
-                        containers=container_list,
-                        restartPolicy=job_event.spec.template.spec.restartPolicy
-                    ),
-                ),
-
-            ),
+    if job_event.spec.template.spec.containers[0].resources.requests['memory'] == params.max_resource:
+        function_name = "job_restart"
+        finding = Finding(
+            title=f"JOB RESTART",
+            source=FindingSource.MANUAL,
+            aggregation_key=function_name,
+            finding_type=FindingType.REPORT,
+            failure=False,
         )
-        job_event.delete()
-        job_spec.create()
+        job_temp = event.get_job()
 
+        finding.add_enrichment(
+            [
+                MarkdownBlock(
+                    f"*job*restart*\n```\n{job_temp}\n```"
+                ),
+            ]
+        )
+        event.add_finding(finding)
+
+        job_event = event.get_job()
+        pod = get_job_pod(event.get_job().metadata.namespace,
+                            event.get_job().metadata.name)
+        
+        status_flag = False
+        # for multi-containers
+        for status in pod.status.containerStatuses:
+            if status.state.terminated.reason == 'OOMKilled':
+                status_flag = True
+                break
+
+        if status_flag:
+            print("han bhai theek hy")         
+            container_list = get_container_list(
+                job_event.spec.template.spec.containers , increase_to=params.increase_to)
+                
+            job_spec = RobustaJob(
+                metadata=ObjectMeta(
+                    name=job_event.metadata.name,
+                    namespace=job_event.metadata.namespace,
+                    labels=job_event.metadata.labels,
+                ),
+                spec=JobSpec(
+                    completions=job_event.spec.completions,
+                    parallelism=job_event.spec.parallelism,
+                    backoffLimit=job_event.spec.backoffLimit,
+                    activeDeadlineSeconds=job_event.spec.activeDeadlineSeconds,
+                    ttlSecondsAfterFinished=job_event.spec.ttlSecondsAfterFinished,
+                    template=PodTemplateSpec(
+                        spec=PodSpec(
+                            containers=container_list,
+                            restartPolicy=job_event.spec.template.spec.restartPolicy
+                        ),
+                    ),
+
+                ),
+            )
+            job_event.delete()
+            job_spec.create()
+    else:
+        print('max reached')
       
 
 
