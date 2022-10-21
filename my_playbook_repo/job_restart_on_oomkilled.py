c180dddba264bf428c5d6387b0b57658db17177c
wahajXgrid <wahaj.ali@xgrid.co>
2022-10-21 14:20:52 +0500

    updated

--- my_playbook_repo/job_restart_on_oomkilled.py
+++ my_playbook_repo/job_restart_on_oomkilled.py
@@ -40,7 +40,7 @@ def job_restart_on_oomkilled(event: JobEvent, params: IncreaseResources):
     """
     try:    
         pod = get_job_latest_pod(job_event)
-    
+        print(pod)
     except:
         logging.error(
             f"get_job_pod was called on event without job: {event}")
@@ -69,7 +69,7 @@ def job_restart_on_oomkilled(event: JobEvent, params: IncreaseResources):
     # )
     if status_flag:
         for i in index: 
-            if max_res[i] < params.max_resource:
+            if float(max_res) < params.max_resource:
                     job_spec = restart_job(job_event, params.increase_by, params.max_resource, i)
 
                     job_temp = job_spec.spec.template.spec.containers[i].resources.requests[
