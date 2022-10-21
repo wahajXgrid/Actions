2ade977559959fa76b93212c0bf6f825c5027ea6
wahajXgrid <wahaj.ali@xgrid.co>
2022-10-21 17:10:44 +0500

    updated

--- my_playbook_repo/job_restart_on_oomkilled.py
+++ my_playbook_repo/job_restart_on_oomkilled.py
@@ -57,6 +57,7 @@ def job_restart_on_oomkilled(event: JobEvent, params: IncreaseResources):
             if status.state.terminated.reason == oom_killed:
                 print(ind)
                 print(pod.spec.containers)
+                print(pod.status.containerStatuses)
                 # index.append(ind)
                 # status_flag = True
                 # max_res.append(PodContainer.get_requests(job_event.spec.template.spec.containers[ind]).memory)
