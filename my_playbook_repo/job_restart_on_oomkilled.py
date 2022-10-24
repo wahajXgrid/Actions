109f517abb702417e7bd1a2d9903bd92ad5620b9
wahajXgrid <wahaj.ali@xgrid.co>
2022-10-24 20:05:38 +0500

    updated

--- my_playbook_repo/job_restart_on_oomkilled.py
+++ my_playbook_repo/job_restart_on_oomkilled.py
@@ -58,14 +58,11 @@ def job_restart_on_oomkilled(event: JobEvent, params: IncreaseResources):
     Retrieves pod's container information for an OOMKilled pod
     """
     OOMKilled = "OOMKilled"
-    #for status in pod.status.containerStatuses:
-    for status in pod:
-        if status.status.containerStatuses == None:
+    for status in pod.status.containerStatuses:
+        if status.state.running == None:
             if status.state.terminated.reason == OOMKilled:
                 oomkilled_container_names.append(status.name)
-        else:
-            running_containers
-    
+                
     print(running_containers)
     for index,container in enumerate(pod.spec.containers):
       
