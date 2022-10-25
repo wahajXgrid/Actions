6f4a8da67ec7b2f0763b5ed4380659c477236b10
wahajXgrid <wahaj.ali@xgrid.co>
2022-10-25 13:56:55 +0500

    updated

--- my_playbook_repo/job_restart_on_oomkilled.py
+++ my_playbook_repo/job_restart_on_oomkilled.py
@@ -80,7 +80,7 @@ def job_restart_on_oomkilled(event: JobEvent, params: IncreaseResources):
     
     job_spec = restart_job(job_event,container_list_after_resource_increment)
 
-    job_spec.spec.template.spec.containers.append(running_containers) 
+    job_spec.spec.template.spec.containers.extend(running_containers) 
     print(job_spec.spec.template.spec.containers)
     job_spec.create()
 
