11fd2780511221b36094707dc00dfbe986d863b6
wahajXgrid <wahaj.ali@xgrid.co>
2022-11-15 19:39:53 +0500

    updated

--- my_playbook_repo/job_restart_on_oomkilled.py
+++ my_playbook_repo/job_restart_on_oomkilled.py
@@ -190,14 +190,24 @@ def memory_increment(resources, increase_by, max_resource, keep_the_same):
         #     return resources
         else:
             if req_unit == 'Mi':
-              if split_increased_memory_unit == 'Gi':
-                split_req = GiB(int(split_increased_memory)).to_MiB() + MiB(int(split_req))
-                split_req = int(split_req)
-                if split_req > float(split_lim):
-                    split_lim = split_req
-                if split_req > max_resource:
-                    split_req = max_resource
-                return ResourceRequirements(limits={"memory": (str(split_lim) + lim_unit)},requests={"memory": (str(split_req) + req_unit)},)
+                if split_increased_memory_unit == 'Gi' or split_increased_memory_unit == 'GiB':
+                    split_req = GiB(int(split_increased_memory)).to_MiB() + MiB(int(split_req))
+                    print(split_req)
+                    split_req = int(split_req)
+                    print(split_req)
+                    if split_req > float(split_lim):
+                        split_lim = split_req
+                    if split_req > max_resource:
+                        split_req = max_resource
+                    return ResourceRequirements(limits={"memory": (str(split_lim) + lim_unit)},requests={"memory": (str(split_req) + req_unit)},)
+                elif split_increased_memory_unit == 'Ki' or split_increased_memory_unit == 'KiB':
+                    split_req = KiB(int(split_increased_memory)).to_KiB() + KiB(int(split_req))
+                    if split_req > float(split_lim):
+                        split_lim = split_req
+                    if split_req > max_resource:
+                        split_req = max_resource
+                    return ResourceRequirements(limits={"memory": (str(split_lim) + lim_unit)},requests={"memory": (str(split_req) + req_unit)},)                
+
             else:
                 return resources
 
