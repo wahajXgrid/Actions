4596a95f903fee37b461753f727ed4f5108269b9
wahajXgrid <wahaj.ali@xgrid.co>
2022-11-15 16:28:43 +0500

    updated

--- my_playbook_repo/job_restart_on_oomkilled.py
+++ my_playbook_repo/job_restart_on_oomkilled.py
@@ -172,6 +172,7 @@ def memory_increment(resources, increase_by, max_resource, keep_the_same):
         
         # Checking if provided unit is same as job's memory unit
         if req_unit == memory_unit:
+            print("yes")
             split_req = float(split_req) + float(split_memory_increment)
 
             if split_req > float(split_lim):
