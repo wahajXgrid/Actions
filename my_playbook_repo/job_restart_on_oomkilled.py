9c6657ecd459fdb5a86bcd95a0e1265f3ce234e6
wahajXgrid <wahaj.ali@xgrid.co>
2022-11-17 13:16:57 +0500

    updated

--- my_playbook_repo/job_restart_on_oomkilled.py
+++ my_playbook_repo/job_restart_on_oomkilled.py
@@ -224,7 +224,7 @@ def memory_increment(resources, increase_by, max_resource, keep_the_same):
                     return ResourceRequirements(limits={"memory": (str(split_lim) + lim_unit)},requests={"memory": (str(split_req) + req_unit)},) 
             if req_unit == 'Ki':
                 if split_increased_memory_unit == 'Mi' or split_increased_memory_unit == 'MiB':
-                    split_req = MiB(int(round(split_increased_memory))).to_KiB() + KiB(int(split_req))
+                    split_req = MiB(int(split_increased_memory)).to_KiB() + KiB(int(split_req))
                     split_req = int(split_req)
                     if split_req > int(split_lim):
                         split_lim = split_req
