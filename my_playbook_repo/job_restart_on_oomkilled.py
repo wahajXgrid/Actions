2bcf3608a1fa624f15da5ab30444081ef45b5857
wahajXgrid <wahaj.ali@xgrid.co>
2022-11-24 18:48:48 +0500

    updated

--- my_playbook_repo/job_restart_on_oomkilled.py
+++ my_playbook_repo/job_restart_on_oomkilled.py
@@ -65,10 +65,8 @@ def job_restart_on_oomkilled(event: JobEvent, params: IncreaseResources):
         if container.name in oomkilled_containers:
             req_memory = PodContainer.get_requests(
                 job_event.spec.template.spec.containers[index]
-            )
-            print(PodResources.pod_requests)
-            
-            print(req_memory)
+            ).memory
+            print(container.resources.request)
           
             # max_resource = bitmath.parse_string_unsafe(params.max_resource)
             # req_memory = bitmath.parse_string_unsafe(PodContainer.get_requests(
