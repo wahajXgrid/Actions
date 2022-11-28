121eb63a57ae1e5cdd3d26dd872677cb53cbeaf8
wahajXgrid <wahaj.ali@xgrid.co>
2022-11-28 12:31:51 +0500

    updated

--- my_playbook_repo/job_restart_on_oomkilled.py
+++ my_playbook_repo/job_restart_on_oomkilled.py
@@ -210,7 +210,7 @@ def memory_increment(resources, increase_by, max_resource, keep_the_same):
 
             elif increase_by.unit == "Ki" or increase_by.unit == "KiB":
                 reqests = increase_by.to_MiB() + reqests
-    
+                print(reqests)
                 reqests = bitmath.MiB(int(float(reqests)))
                 if reqests > max_resource:
                     print("no")
