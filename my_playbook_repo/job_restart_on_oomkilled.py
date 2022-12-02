7a65c16a59a9fffce7bd7e413eae7e62b590a034
wahajXgrid <wahaj.ali@xgrid.co>
2022-12-02 16:05:40 +0500

    updated

--- my_playbook_repo/job_restart_on_oomkilled.py
+++ my_playbook_repo/job_restart_on_oomkilled.py
@@ -186,10 +186,13 @@ def memory_increment(resources, increase_by, max_resource, keep_the_same):
                 )
 
             elif increase_by.unit == "Gi" or increase_by.unit == "GiB":
+                print("called")
                 reqests = increase_by + reqests             
                 if reqests > max_resource:
+                    print("called 1")
                     reqests = max_resource.to_MiB()
                 if reqests > limits:
+                    print("called 2")
                     limits = reqests
 
                 return ResourceRequirements(
