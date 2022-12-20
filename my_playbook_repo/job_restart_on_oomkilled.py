38fcf561ac928d3b0aebc31e1e929ccac988c8a9
wahajXgrid <wahaj.ali@xgrid.co>
2022-12-20 13:49:30 +0500

    updated

--- my_playbook_repo/job_restart_on_oomkilled.py
+++ my_playbook_repo/job_restart_on_oomkilled.py
@@ -173,12 +173,14 @@ def memory_increment(resources, increase_by, max_resource, keep_the_same):
         increase_by = bitmath.parse_string_unsafe(increase_by)
         #requests = increase_by + requests
         requests = min((increase_by + requests) , max_resource)
+        print(requests)
         # Restricting requests memory to exceed the max_resource given by user
         # if requests > max_resource:
         #     requests = max_resource
 
         # increasing limits memory accordingly
         limits = min(requests,limits)
+        print(limits)
         # if requests > limits:
         #     limits = requests
         formatted_request = requests.format("{value:.0f}{unit}").rstrip("B")
