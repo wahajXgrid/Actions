df108edd5d89b8189fac95280a02d7f5f9942b83
wahajXgrid <wahaj.ali@xgrid.co>
2023-02-08 19:19:25 +0500

    a

--- my_playbook_repo/delete_unclaimed_volume.py
+++ my_playbook_repo/delete_unclaimed_volume.py
@@ -2,11 +2,19 @@ from robusta.api import *
 
 @action
 def delete_unclaimed_volume(event: PodEvent):
-    """
-    Deletes a pod
-    """
-    if not event.get_pod():
-        logging.info("Failed to get the pod for deletion")
-        return
-
-    event.get_pod().delete()
+    function_name = "delete_unclaimed_volume"
+    finding = Finding(
+        title=f"AD",
+        source=FindingSource.MANUAL,
+        aggregation_key=function_name,
+        finding_type=FindingType.REPORT,
+        failure=False,
+    )
+    finding.add_enrichment(
+        [
+            MarkdownBlock(
+                f"*containers memory after restart"
+            ),
+        ]
+    )
+    event.add_finding(finding)
